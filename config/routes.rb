09a5db32d9dd9a9b5a7e45d20487a2fb050e9468
danajackson2 <danajackson2@gmail.com>
2020-12-18 14:47:51 -0800

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :trips, only: [:new, :create, :edit, :update, :show]
   resources :trails, only: [:index, :show]
   resources :hikers
+  get '/home', to: 'home#show'
 end
