8d3963854371216ef8622acd61012668ddad8504
danajackson2 <danajackson2@gmail.com>
2020-12-18 14:48:53 -0800

    hiker-app in url

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :trips, only: [:new, :create, :edit, :update, :show]
   resources :trails, only: [:index, :show]
   resources :hikers
-  get '/home', to: 'home#show'
+  get '/hiker-app', to: 'home#show'
 end
